d562fdba324485dc4be1c08427f340b49dadb079
Peter Sinkovicz <antsticky@gmail.com>
2025-12-23 09:17:10 +0100

    sane

--- api/main.py
+++ api/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello World from FastAPI on Vercel!"}
+
+@app.get("/sane")
+def hello():
+    return {"message": "I am working sane!"}
